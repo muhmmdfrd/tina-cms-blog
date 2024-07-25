f4df71934e70b32c592ed660226556dc3b5cff90
Muhammad Farid <mfarid2121@gmail.com>
2024-07-25 21:14:16 +0700

    baru hehe

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,11 +9,11 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
+	author: 'Muhammad Farid',
+	title: 'Bithubby Author',
+	description: 'A passionate developer who loves turning ideas into code. Love music and writing',
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
-	shareMessage: 'Share this post', // Message to share a post on social media
-	paginationSize: 6 // Number of posts per page
+	shareMessage: 'Share this post',
+	paginationSize: 6
 }
